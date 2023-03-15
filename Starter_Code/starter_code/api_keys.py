2d13662dd9b0fd67202c22a384d4a290a87558de
meg vogel <megvogel@Meg-Vogel-2.local>
2023-03-14 17:00:46 -0700

    API call

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "c6b24c6ef455db69bf0f46d5ca661347"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ce9cc9a32e3b4f8da190ac60a68f26f3"
